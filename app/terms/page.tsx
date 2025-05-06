ba59d3b6e65e7527810ff3cd2ea0a2c25e95785e
Andrew <113452664+ranareinsit@users.noreply.github.com>
2025-05-06 14:57:03 +0800

    refactor(legal-pages): improving the styles and structure of legal pages
    Updated styles for official appearance
    Page content moved to separate entities

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { Box, Stack, Container, Typography } from "@mui/material";
-import { useColorScheme } from "@mui/material/styles";
+import { Box, Stack, Container, Typography, Toolbar } from "@mui/material";
 import Section from "@/app/_components/Section";
 import SectionHeader from "@/app/_components/SectionHeader";
 import Navbar from "@/app/_components/Navbar";
@@ -10,29 +9,16 @@ import Paragraph from "@/app/_components/Paragraph";
 import { useI18n } from "@/app/_providers/I18nProvider";
 import ScrollTop from "@/app/_components/ScrollTop";
 
-export default function TermsPage() {
+
+function PageContent() {
   const { dict } = useI18n();
   const termsContent = dict?.Legal?.Terms;
-  const { mode } = useColorScheme();
-  const isDark = mode === "dark";
-  const bgSelect = isDark ? "var(--mui-palette-primary-dark)" : "var(--mui-palette-primary-light)";
-  const bgSelect2 = isDark
-    ? "rgb(from var(--mui-palette-secondary-dark) r g b / 0.8)"
-    : "rgb(from var(--mui-palette-primary-main) r g b / 0.2)";
-
   if (!termsContent) return null;
-
   return (
-    <Box sx={{ margin: "0 auto", minWidth: "320px", overflowX: "hidden" }}>
-      <div id="back-to-top-anchor" />
+    <>
       <Navbar />
       <Hero fullsize={false} />
-      <Section
-        id="terms"
-        sx={{
-          background: `linear-gradient(0deg, ${bgSelect2} 30%, ${bgSelect})`,
-        }}
-      >
+      <Section id="terms">
         <SectionHeader>
           <Typography
             variant="h3"
@@ -67,7 +53,25 @@ export default function TermsPage() {
         </Container>
       </Section>
       <Footer />
+    </>
+  )
+}
+
+export default function Page() {
+  return (
+    <Box sx={{
+      alignItems: `center`,
+      display: `flex`,
+      flexDirection: `column`,
+      justifyContent: `start`,
+      margin: `0 auto`,
+      overflow: `hidden`,
+      overflowX: `hidden`,
+      width: `100%`,
+    }}>
+      <Toolbar id="back-to-top-anchor" sx={{ position: `absolute` }} />
+      <PageContent />
       <ScrollTop />
     </Box>
-  );
+  )
 }
